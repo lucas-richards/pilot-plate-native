ba8b762b52abadc6f14b4bbc6e032b149f7f1b34
lucas richards dev <lucasrichardsdev@gmail.com>
2023-11-13 13:10:17 -0800

    header

--- App.js
+++ App.js
@@ -3,19 +3,28 @@ import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import React from 'react';
 import Header from './components/Header';
+import NavBar from './components/NavBar';
+import { NavigationContainer } from '@react-navigation/native';
+
+
 
 
 export default function App() {
   return (
+    
+    <NavigationContainer>
         <LinearGradient
-      colors={['rgb(239, 120, 36)', 'rgb(236, 80, 31)']}
-      style={styles.container}
-    >
-      <SafeAreaView>
-        <Header />
-        <StatusBar style="auto" />
-      </SafeAreaView>
-    </LinearGradient>
+          colors={['rgb(239, 120, 36)', 'rgb(236, 80, 31)']}
+          style={styles.container}
+        >
+          <SafeAreaView>
+            <StatusBar style="auto" />
+              <Header />
+          </SafeAreaView>
+        </LinearGradient>
+              
+    
+      </NavigationContainer>
 
   );
 }
