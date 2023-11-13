e5db075c6c78ab593a0eb3b397848836e6b42cb2
lucas richards dev <lucasrichardsdev@gmail.com>
2023-11-13 09:53:21 -0800

    gradient

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <LinearGradient
+      colors={['rgb(239, 120, 36)', 'rgb(236, 80, 31)']}
+      style={styles.container}
+    >
       <Text>Plate Pilot App!</Text>
       <StatusBar style="auto" />
-    </View>
+      {/* Your content */}
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
