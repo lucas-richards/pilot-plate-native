f816df11a32ddbe9d9e9dc251a6d15eabad8d587
lucas richards dev <lucasrichardsdev@gmail.com>
2023-11-16 18:11:23 -0800

    delete comments in profile

--- pages/Profile.tsx
+++ pages/Profile.tsx
@@ -58,10 +58,7 @@ const Profile = ({ navigation }) => {
 
 
 
-          {/* <SignInForm /> */}
-          {/* <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Profile!</Text>
-          </View> */}
+          
         </LinearGradient> 
       </>
   
