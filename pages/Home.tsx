90867b39106553e07e0f74969eae30724758a574
lucas richards dev <lucasrichardsdev@gmail.com>
2023-11-22 19:11:59 -0800

    more info

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -19,6 +19,9 @@ const Home = () => {
               <Text style={{marginHorizontal: 10}}>
                   restaurant info
               </Text>
+              <Text style={{marginHorizontal: 10}}>
+                  more info
+              </Text>
               <Button
                 onPress={() => alert('view restaurant!')}
                 icon={<Icon name='code' color='#ffffff' />}
