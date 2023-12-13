0f808915631e57d6d6e14cd821a06e240f14d7d9
lucas richards dev <lucasrichardsdev@gmail.com>
2023-12-12 21:17:49 -0800

    added some details

--- pages/business/Detail.tsx
+++ pages/business/Detail.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { StyleSheet, View, Text, Button, Image, Touchable, TouchableOpacity } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { StarRatingDisplay } from 'react-native-star-rating-widget';
@@ -7,9 +7,11 @@ import {Card} from 'react-native-elements';
 import { FontAwesome } from '@expo/vector-icons';
 import HeartFavorite from '../../components/HeartFavorite';
 import { AntDesign } from '@expo/vector-icons'; 
+import { DbContext } from '../../DataContext';
 
 const Detail = ({route, navigation:{goBack}}) => {
     const business = route.params.selectedBusiness
+    const { user } = useContext(DbContext);
     
     let businessAddress = business.display_address || business.location.display_address
 
@@ -49,30 +51,51 @@ const Detail = ({route, navigation:{goBack}}) => {
                     <Card.Image style={styles.cardImage} source={{ uri: `${business.image_url}` }} />
                         
                     <Card.Divider/>
-                    <Text style={{marginHorizontal: 10, fontWeight:'600'}}>
-                        {business.name}
+                    <Text style={{fontSize:20,marginHorizontal: 10, fontWeight:'600'}}>
+                        {business.name} - {business.is_closed?
+                            <Text style={{color:'red'}}>Closed</Text>
+                            :
+                            <Text style={{color:'green'}}>Open</Text>}
                     </Text>
-                    <View style={{flexDirection: 'row', alignItems:'center',justifyContent:'space-between', marginHorizontal: 10, marginTop:5}}>
-                        <Text style={{fontSize:20}}>{ typeof business.price === 'number'?'$'.repeat(business.price):business.price}</Text>
-                            <Text style={{marginHorizontal: 10}}>
-                            {/* {business.rating} */}
-                            <StarRatingDisplay rating={business.rating} starSize={25}/>
-                            </Text>
+                    <Text style={{fontSize:20, marginLeft:10}}>{ typeof business.price === 'number'?'$'.repeat(business.price):business.price}</Text>
+                    <View style={{flexDirection: 'row', alignItems:'center', marginHorizontal: 10, marginTop:5}}>
+                        <Text style={{color:'gray'}}>
+                            {/* if business coming from yelp show all reviews, if not user review */}
+                            ({typeof business.price === 'number'? user.email:business.review_count})
+                        </Text>
+                        <StarRatingDisplay rating={business.rating} starSize={25}/> 
                     </View>
+                    
                     <View style={{margin:10}}>
+                        <Text style={{marginTop:5}}>
+                            {Math.round((business.distance / 1609) * 10) / 10} mi away
+                        </Text>
+                        <Text
+                            style={{marginTop:5}}
+                            onPress={() => handleOpenMaps(businessAddress)}
+                        >
+                            <Text style={{fontWeight:'bold'}}>
+                                Address:
+                            </Text> 
+                            {businessAddress}
+                        </Text>
+                        
                         
                         <Text
-                            onPress={() => handleOpenMaps(businessAddress)}
-                        >Address: {businessAddress}</Text>
-                        
-                        
-                            <Text>
-                                <Text 
-                                    style={{fontWeight:'400'}}
-                                    onPress={()=> dialCall(`${business.display_phone}`)}
-                                >Phone:</Text>
-                                {business.display_phone}
+                            style={{marginTop:5}}
+                        >
+                            <Text 
+                                style={{fontWeight:'bold'}}
+                                onPress={()=> dialCall(`${business.display_phone}`)}
+                            >
+                                Phone:
                             </Text>
+                            {business.display_phone}
+                        </Text>
+                        
+                            
+                        
+                        
                         
                     </View>
                 </View>
@@ -105,7 +128,7 @@ const styles = StyleSheet.create({
     card: {
         backgroundColor:'white',
         width: 300,
-        height: 400,
+        height: 500,
         borderRadius:10
       },
       cardImage: {
